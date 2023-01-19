a4bbb21db6c841430fc10f4228013f4028dc50a6
DerikOmond <derik.omond@gmail.com>
2023-01-19 15:41:31 +0000

    Player Displacement
    Using keys 'w', 'a', 's' and 'd', the player's x and y coordinates are translated by the value of the integer 'speed'.

--- Game file/C0452-Game-Project/Player.java
+++ Game file/C0452-Game-Project/Player.java
@@ -12,11 +12,31 @@ public class Player extends Actor
      * Act - do whatever the Player wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    int speed = 5;
     public Player(){
         getImage().scale(getImage().getWidth()/3, getImage().getHeight()/3);
     }
     public void act()
     {
-        // Add your action code here.
+        movement();
+    }
+    
+    public void movement()
+    {
+        if(Greenfoot.isKeyDown("w"))
+        {
+            setLocation(getX(), getY() - speed);
+        }
+        if(Greenfoot.isKeyDown("s"))
+        {
+            setLocation(getX(), getY() + speed);
+        }
+        if(Greenfoot.isKeyDown("d"))
+        {
+            setLocation(getX() + speed, getY());
+        }if(Greenfoot.isKeyDown("a"))
+        {
+            setLocation(getX() - speed, getY());
+        }
     }
 }
