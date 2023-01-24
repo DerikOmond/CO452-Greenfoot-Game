077ea50d2346435bcc7e9d27977ca1d37e5232e2
DerikOmond <derik.omond@gmail.com>
2023-01-24 09:04:09 +0000

    Update project.greenfoot

---
+++ Game file/C0452-Game-Project/GifActor.java
@@ -0,0 +1,35 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class GifActor here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class GifActor extends Actor
+{   private boolean isFirstImage;
+    private GreenfootImage firstImage; 
+    GifImage myGif1 = new GifImage("gif1.gif");
+    /**
+     * Act - do whatever the GifActor wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        setLocation(786,757);
+        setImage(myGif1.getCurrentImage());
+        if (firstImage == null) // initializing (only executed during first act step)
+    {
+    firstImage = getImage(); // retain first image
+    isFirstImage = true; // set tracking field
+     }
+     if (isFirstImage != (getImage() == firstImage)) // if an image change involved first image
+    {
+    isFirstImage = ! isFirstImage; // toggle tracking field
+    if (isFirstImage) getWorld().removeObject(this);
+    // if returning to first image, remove this
+    }
+    }
+   
+    
+}
